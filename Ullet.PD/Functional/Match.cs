f1cd0fd441e8f758c1ba64fd99a92df99271bc41
Trevor Barnett <mr.ullet@gmail.com>
2015-08-02 17:12:00 +0100

    Matcher type with switch-like behaviour

---
+++ Ullet.PD/Functional/Match.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Written by Trevor Barnett, <mr.ullet@gmail.com>, 2015
+ * Released to the Public Domain.  See http://unlicense.org/ or the
+ * UNLICENSE file accompanying this source code.
+ */
+
+namespace Ullet.PD.Functional
+{
+  /// <summary>
+  /// Match value to conditionally return a result.
+  /// </summary>
+  public static class Match<TOn, TResult>
+  {
+    /// <summary>
+    /// Value to match against.
+    /// </summary>
+    public static Matcher<TOn, TResult> On(TOn value)
+    {
+      return new Matcher<TOn, TResult>(value);
+    }
+
+    /// <summary>
+    /// Match an expression with cases to be specified.
+    /// </summary>
+    public static Matcher<TOn, TResult> With
+    {
+      get { return new Matcher<TOn, TResult>(); }
+    }
+  }
+}
