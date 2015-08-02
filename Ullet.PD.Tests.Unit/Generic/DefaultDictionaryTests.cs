03882e32c434f76cfabe9c16db15955b1ee5476c
Trevor Barnett <mr.ullet@gmail.com>
2015-08-02 15:51:45 +0100

    Dictionary-like class returning default instead of exception

---
+++ Ullet.PD.Tests.Unit/Generic/DefaultDictionaryTests.cs
@@ -0,0 +1,330 @@
+﻿/*
+ * Written by Trevor Barnett, <mr.ullet@gmail.com>, 2015
+ * Released to the Public Domain.  See http://unlicense.org/ or the
+ * UNLICENSE file accompanying this source code.
+ */
+
+using System.Collections.Generic;
+using NUnit.Framework;
+using Ullet.PD.Generic;
+
+namespace Ullet.PD.Tests.Unit.Generic
+{
+  [TestFixture]
+  public class DefaultDictionaryTests
+  {
+    [Test]
+    public void CanAddKeyAndValueToDictionary()
+    {
+      var dictionary =
+        new DefaultDictionary<string, string> { { "key", "value" } };
+
+      Assert.That(dictionary["key"], Is.EqualTo("value"));
+    }
+
+    [Test]
+    public void CanAddMultipleKeysAndValuesToDictionary()
+    {
+      var dictionary = new DefaultDictionary<string, string>
+        {
+          {"key1", "value1"},
+          {"key2", "value2"},
+          {"key3", "value3"}
+        };
+
+      Assert.That(
+        new[] { dictionary["key1"], dictionary["key2"], dictionary["key3"] },
+        Is.EqualTo(new[] { "value1", "value2", "value3" }));
+    }
+
+    [Test]
+    public void AddReturnsFalseIfKeyAlreadyAdded()
+    {
+      var dictionary = new DefaultDictionary<string, string>
+        {
+          {"key", "value"}
+        };
+
+      var result = dictionary.Add("key", "other-value");
+
+      Assert.That(result, Is.False);
+    }
+
+    [Test]
+    public void DoesNotAddElementIfKeyAlreadyAdded()
+    {
+      var dictionary = new DefaultDictionary<string, string>
+        {
+          {"key", "value"},
+          {"key", "other-value"}
+        };
+
+      Assert.That(dictionary, Has.Count.EqualTo(1));
+      Assert.That(dictionary["key"], Is.EqualTo("value"));
+    }
+
+    [Test]
+    public void AddReturnsTrueIfSuccessfullyAdded()
+    {
+      var dictionary = new DefaultDictionary<string, string>();
+
+      var result = dictionary.Add("key", "value");
+
+      Assert.That(result, Is.True);
+      Assert.That(dictionary["key"], Is.EqualTo("value"));
+    }
+
+    [Test]
+    public void CanClearAllElements()
+    {
+      var dictionary = new DefaultDictionary<string, string>
+        {
+          {"key", "value"}
+        };
+
+      dictionary.Clear();
+
+      Assert.That(dictionary, Is.Empty);
+    }
+
+    [Test]
+    public void CanTestContainsKeyWhenKeyExists()
+    {
+      var dictionary = new DefaultDictionary<string, string>
+        {
+          {"key", "value"}
+        };
+
+      Assert.That(dictionary.ContainsKey("key"), Is.True);
+    }
+
+    [Test]
+    public void CanTestContainsKeyWhenKeyDoesNotExist()
+    {
+      var dictionary = new DefaultDictionary<string, string>
+        {
+          {"key", "value"}
+        };
+
+      Assert.That(dictionary.ContainsKey("other-key"), Is.False);
+    }
+
+    [Test]
+    public void NeverContainsNullKey()
+    {
+      var dictionary = new DefaultDictionary<string, string>();
+
+      Assert.That(dictionary.ContainsKey(null), Is.False);
+    }
+
+    [Test]
+    public void CanCountNumberOfElements()
+    {
+      var dictionary = new DefaultDictionary<string, string>
+        {
+          {"key1", "value1"},
+          {"key2", "value2"},
+          {"key3", "value3"}
+        };
+
+      Assert.That(dictionary.Count, Is.EqualTo(3));
+    }
+
+    [Test]
+    public void HasEnumeratorOfKeyValuePairs()
+    {
+      var enumerator = new DefaultDictionary<string, string>
+        {
+          {"key1", "value1"},
+          {"key2", "value2"},
+          {"key3", "value3"}
+        }.GetEnumerator();
+      var enumerated = new List<KeyValuePair<string, string>>();
+
+      while (enumerator.MoveNext())
+        enumerated.Add(enumerator.Current);
+
+      Assert.That(
+        enumerated,
+        Is.EquivalentTo(new[]
+          {
+            new KeyValuePair<string, string>("key1", "value1"),
+            new KeyValuePair<string, string>("key2", "value2"),
+            new KeyValuePair<string, string>("key3", "value3")
+          }));
+    }
+
+    [Test]
+    public void CanGetCollectionOfKeys()
+    {
+      var dictionary = new DefaultDictionary<string, string>
+        {
+          {"key1", "value1"},
+          {"key2", "value2"},
+          {"key3", "value3"}
+        };
+
+      var keys = dictionary.Keys;
+
+      Assert.That(keys, Is.EquivalentTo(new[] { "key1", "key2", "key3" }));
+    }
+
+    [Test]
+    public void CanRemoveElementByItsKey()
+    {
+      var dictionary = new DefaultDictionary<string, string>
+        {
+          {"key1", "value1"},
+          {"key2", "value2"},
+          {"key3", "value3"}
+        };
+
+      dictionary.Remove("key2");
+
+      Assert.That(
+        dictionary,
+        Is.EquivalentTo(
+          new Dictionary<string, string>
+            {
+              {"key1", "value1"},
+              {"key3", "value3"}
+            }));
+    }
+
+    [Test]
+    public void RemoveReturnsTrueIfElementisRemoved()
+    {
+      var dictionary = new DefaultDictionary<string, string>
+        {
+          {"key1", "value1"},
+          {"key2", "value2"},
+          {"key3", "value3"}
+        };
+
+      Assert.That(dictionary.Remove("key2"), Is.True);
+    }
+
+    [Test]
+    public void RemoveReturnsFalseIfKeyNotFound()
+    {
+      var dictionary = new DefaultDictionary<string, string>();
+
+      Assert.That(dictionary.Remove("key"), Is.False);
+    }
+
+    [Test]
+    public void NothingRemovedIfKeyIsNull()
+    {
+      var dictionary = new DefaultDictionary<string, string>
+        {
+          {"key1", "value1"},
+          {"key2", "value2"},
+          {"key3", "value3"}
+        };
+
+      dictionary.Remove(null);
+
+      Assert.That(
+        dictionary,
+        Is.EquivalentTo(
+          new Dictionary<string, string>
+            {
+              {"key1", "value1"},
+              {"key2", "value2"},
+              {"key3", "value3"}
+            }));
+    }
+
+    [Test]
+    public void IndexerAddsNewElementForNewKey()
+    {
+      var dictionary = new DefaultDictionary<string, string>();
+
+      dictionary["key"] = "value";
+
+      Assert.That(dictionary["key"], Is.EqualTo("value"));
+    }
+
+    [Test]
+    public void IndexerOverwritesValueForExistingKey()
+    {
+      var dictionary = new DefaultDictionary<string, string>
+        {
+          {"key", "value"}
+        };
+
+      dictionary["key"] = "other-value";
+
+      Assert.That(dictionary["key"], Is.EqualTo("other-value"));
+    }
+
+    [Test]
+    public void IndexerAddsNothingIfKeyIsNull()
+    {
+      var dictionary = new DefaultDictionary<string, string>
+        {
+          {"key", "value"}
+        };
+
+      dictionary[null] = "other-value";
+
+      Assert.That(dictionary, Has.Count.EqualTo(1));
+      Assert.That(dictionary["key"], Is.EqualTo("value"));
+    }
+
+    [Test]
+    public void CanGetValueOfElementByItsKey()
+    {
+      var dictionary = new DefaultDictionary<string, string>
+        {
+          {"key1", "value1"},
+          {"key2", "value2"},
+          {"key3", "value3"}
+        };
+
+      Assert.That(dictionary["key2"], Is.EqualTo("value2"));
+    }
+
+    [Test]
+    public void IndexerReturnsDefaultForTypeIfKeyNotFound()
+    {
+      // ReSharper disable once CollectionNeverUpdated.Local
+      var dictionary = new DefaultDictionary<string, string>();
+
+      Assert.That(dictionary["missing-key"], Is.EqualTo(null));
+    }
+
+    [Test]
+    public void IndexerReturnsDefaultForAnotherTypeIfKeyNotFound()
+    {
+      // ReSharper disable once CollectionNeverUpdated.Local
+      var dictionary = new DefaultDictionary<string, int>();
+
+      Assert.That(dictionary["missing-key"], Is.EqualTo(0));
+    }
+
+    [Test]
+    public void IndexerReturnsDefaultIfKeyNull()
+    {
+      // ReSharper disable once CollectionNeverUpdated.Local
+      var dictionary = new DefaultDictionary<string, int>();
+
+      Assert.That(dictionary[null], Is.EqualTo(0));
+    }
+
+    [Test]
+    public void CanGetCollectionOfValues()
+    {
+      var dictionary = new DefaultDictionary<string, string>
+        {
+          {"key1", "value1"},
+          {"key2", "value2"},
+          {"key3", "value3"}
+        };
+
+      var keys = dictionary.Values;
+
+      Assert.That(keys, Is.EquivalentTo(new[] {"value1", "value2", "value3"}));
+    }
+  }
+}
---
+++ Ullet.PD/Generic/DefaultDictionary.cs
@@ -0,0 +1,194 @@
+﻿/*
+ * Written by Trevor Barnett, <mr.ullet@gmail.com>, 2015
+ * Released to the Public Domain.  See http://unlicense.org/ or the
+ * UNLICENSE file accompanying this source code.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Ullet.PD.Generic
+{
+  /// <summary>
+  /// Dictionary of keys and values returning a default value when key not
+  /// found.
+  /// </summary>
+  /// <remarks>
+  /// Looking up a value for a key not in the dictionary returns default rather
+  /// than throw an exception.
+  /// Does NOT implement interface <see cref="T:IDictionary{TKey, TValue}"/>.
+  /// The dictionary interface expects exceptions to be thrown for missing and
+  /// null keys, but this class intentionally breaks that convention.
+  /// Unlike the IDictionary interface, this class does not have a TryGetValue
+  /// method. This is because the indexer does not throw an exception if the key
+  /// is not found, so not expensive to try using an invalid key with the
+  /// indexer, and the only reason to use this class, instead of a standard
+  /// <see cref="T:Dictionary{TKey, TValue}"/>, is because you don't really care
+  /// if the key exists or not; you just want to use a default value if it does
+  /// not. A <see cref="ContainsKey"/> method is available if do need to
+  /// explicitly check if a key exists.
+  /// </remarks>
+  /*
+   * An explicit interface implementation could be used to "preserve"
+   * IDictionary behaviour when cast to the interface, however that would lead
+   * to the confusing situation of very different behaviour when cast to the
+   * interface. The implementation of this class would be overly complicated by
+   * essentially being two implementations in one.  The explicit
+   * IDictionary<TKey, TValue> interface would behave exactly like a standard
+   * Dictionary<TKey, TValue> class, losing everything "special" about this
+   * class.
+   */
+  public class DefaultDictionary<TKey, TValue>
+    : IEnumerable<KeyValuePair<TKey, TValue>>
+  {
+    private readonly IDictionary<TKey, TValue> _innerDictionary;
+
+    /// <summary>
+    /// Create new dictionary of values.
+    /// </summary>
+    public DefaultDictionary()
+      : this(default(TValue))
+    {
+    }
+
+    /// <summary>
+    /// Create new dictionary of values.
+    /// </summary>
+    public DefaultDictionary(TValue defaultValue)
+    {
+      _innerDictionary = new Dictionary<TKey, TValue>();
+      DefaultValue = defaultValue;
+    }
+
+    /// <summary>
+    /// Get or set default value returned if key not found.
+    /// </summary>
+    public TValue DefaultValue { get; set; }
+
+    /// <summary>
+    /// Adds an element with the provided key and value to the dictionary.
+    /// </summary>
+    /// <param name="key">
+    /// The object to use as the key of the element to add.
+    /// </param>
+    /// <param name="value">
+    /// The object to use as the value of the element to add.
+    /// </param>
+    public bool Add(TKey key, TValue value)
+    {
+      if (Equals(null, key) || ContainsKey(key))
+        return false;
+      _innerDictionary.Add(key, value);
+      return true;
+    }
+
+    /// <summary>
+    /// Removes all items from the dictionary.
+    /// </summary>
+    public void Clear()
+    {
+      _innerDictionary.Clear();
+    }
+
+    /// <summary>
+    /// Determines whether the dictionary contains an element with the specified
+    /// key.
+    /// </summary>
+    /// <returns>
+    /// true if the dictionary contains an element with the key;
+    /// otherwise, false.
+    /// </returns>
+    /// <param name="key">The key to locate in the dictionary.</param>
+    public bool ContainsKey(TKey key)
+    {
+      return !Equals(key, null) && _innerDictionary.ContainsKey(key);
+    }
+
+    /// <summary>
+    /// Gets the number of elements contained in the dictionary.
+    /// </summary>
+    /// <returns>
+    /// The number of elements contained in the dictionary.
+    /// </returns>
+    public int Count
+    {
+      get { return _innerDictionary.Count; }
+    }
+
+    /// <summary>
+    /// Returns an enumerator that iterates through the collection.
+    /// </summary>
+    /// <returns>
+    /// An enumerator that can be used to iterate through the collection.
+    /// </returns>
+    public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+    {
+      return _innerDictionary.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+      return GetEnumerator();
+    }
+
+    /// <summary>
+    /// Gets a collection containing the keys of the dictionary.
+    /// </summary>
+    /// <returns>
+    /// A collection containing the keys of the dictionary.
+    /// </returns>
+    public ICollection<TKey> Keys
+    {
+      get { return _innerDictionary.Keys; }
+    }
+
+    /// <summary>
+    /// Removes the element with the specified key from the dictionary.
+    /// </summary>
+    /// <returns>
+    /// true if the element is successfully removed; otherwise, false.
+    /// This method also returns false if <paramref name="key"/> was not
+    /// found in the original dictionary.
+    /// </returns>
+    /// <param name="key">The key of the element to remove.</param>
+    public bool Remove(TKey key)
+    {
+      return !Equals(null, key) && _innerDictionary.Remove(key);
+    }
+
+    /// <summary>
+    /// Gets or sets the element with the specified key.
+    /// </summary>
+    /// <returns>
+    /// The element with the specified key.
+    /// </returns>
+    /// <param name="key">The key of the element to get or set.</param>
+    /// <remarks>
+    /// Returns default value for type if key not found or key is null.
+    /// Element will not be set if key is null, an exception will not be thrown.
+    /// </remarks>
+    public TValue this[TKey key]
+    {
+      get
+      {
+        return ContainsKey(key) ? _innerDictionary[key] : DefaultValue;
+      }
+      set
+      {
+        if (!Equals(null, key))
+          _innerDictionary[key] = value;
+      }
+    }
+
+    /// <summary>
+    /// Gets a collection containing the values in the dictionary.
+    /// </summary>
+    /// <returns>
+    /// A collection containing the values in the dictionary.
+    /// </returns>
+    public ICollection<TValue> Values
+    {
+      get { return _innerDictionary.Values; }
+    }
+  }
+}
